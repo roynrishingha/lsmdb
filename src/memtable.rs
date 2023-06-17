148bbda34fc8b68dd1dfd865bb9ea56c632f9079
roynrishingha <roynrishingha@gmail.com>
2023-06-17 23:23:48 +0530

    Implement MemTable data structure
    The MemTable is implemented using a B-tree map (BTreeMap) provided by the standard library.
    This map allows key-value pairs to be stored in sorted order based on the keys.
    To ensure thread-safety and allow for concurrent access, the entries field of the MemTable is wrapped in an `Arc<Mutex<>>` construct, providing shared ownership and interior mutability.
    
    Unit tests have been included to validate the correctness of the implemented functionality.

--- src/memtable.rs
+++ src/memtable.rs
@@ -1,185 +1,198 @@
-//! # MemTable
-//!
-//! `MemTable` is a memory-based key-value storage that is part of a Log-Structured Merge (LSM) Tree storage engine.
-//! It provides fast read and write operations by maintaining key-value pairs in memory.
-//!
-//! It imports dependencies like:
-//! bytes, which provides an efficient byte buffer implementation,
-//! crossbeam_skiplist, which provides a concurrent skip list data structure.
-//! It also imports Arc from the standard library for atomic reference counting.
-//!
+use std::{
+    collections::BTreeMap,
+    io,
+    sync::{Arc, Mutex},
+};
 
-use crate::write_ahead_log::WriteAheadLog;
-use bytes::Bytes;
-use crossbeam_skiplist::SkipMap;
-use std::{collections::HashSet, io, path::Path, sync::Arc};
+/// Setting default capacity to be 1GB.
+pub(crate) static MEMTABLE_DEFAULT_CAPACITY: usize = 1024;
 
+/// The MemTable is an in-memory data structure that stores recently written data before it is flushed to disk.
 pub(crate) struct MemTable {
-    entries: Arc<SkipMap<Bytes, Bytes>>,
-    deleted_keys: HashSet<Bytes>,
-    wal: WriteAheadLog,
+    /// Stores key-value pairs in sorted order.
+    entries: Arc<Mutex<BTreeMap<Vec<u8>, Vec<u8>>>>,
+    /// The number of key-value entries present in the MemTable.
+    entry_count: usize,
+    /// Current size of the MemTable in bytes.
+    size: usize,
+    /// The maximum allowed size for the MemTable in MB. This is used to enforce size limits and trigger `flush` operations.
+    capacity: usize,
 }
 
 impl MemTable {
-    pub(crate) fn new(log_file_path: &Path) -> io::Result<Self> {
-        let wal = WriteAheadLog::new(log_file_path)?;
-
-        Ok(Self {
-            entries: Arc::new(SkipMap::new()),
-            deleted_keys: HashSet::new(),
-            wal,
-        })
-    }
-
-    /// Get corresponding `value` by `key`
-    pub(crate) fn get(&self, key: &[u8]) -> Option<Bytes> {
-        let key_bytes = Bytes::copy_from_slice(key);
-
-        if self.deleted_keys.contains(&key_bytes) {
-            None
-        } else {
-            self.entries.get(key).map(|entry| entry.value().to_owned())
+    /// Creates a new MemTable with the default MemTable capacity of 1GB.
+    pub(crate) fn new() -> Self {
+        Self {
+            entries: Arc::new(Mutex::new(BTreeMap::new())),
+            entry_count: 0,
+            size: 0,
+            capacity: MEMTABLE_DEFAULT_CAPACITY,
         }
     }
 
-    /// Set a key-value pair into MemTable
-    pub(crate) fn set(&mut self, key: &[u8], value: &[u8]) {
-        let key_bytes = Bytes::copy_from_slice(key);
-        let value_bytes = Bytes::copy_from_slice(value);
-
-        self.deleted_keys.remove(&key_bytes);
-        self.entries.insert(key_bytes, value_bytes);
-    }
-
-    /// Check if the MemTable is empty
-    pub(crate) fn is_empty(&self) -> bool {
-        self.entries.is_empty()
-    }
-
-    /// Remove a key from the MemTable
-    pub(crate) fn remove(&mut self, key: &[u8]) -> Result<(), ()> {
-        let key_bytes = Bytes::copy_from_slice(key);
-
-        if self.deleted_keys.contains(&key_bytes) {
-            // Return an error if the key is already deleted
-            Err(())
-        } else if let Some(value) = self.get(key) {
-            // First delete the key from SkipMap and add the key to deleted list
-            self.entries.remove(key);
-            self.deleted_keys.insert(key_bytes.clone());
-
-            Ok(())
-        } else {
-            // Return an error if the key does not exist
-            Err(())
+    /// Creates a new MemTable with the specified maximum size in MB.
+    pub(crate) fn with_capacity(capacity: usize) -> Self {
+        Self {
+            entries: Arc::new(Mutex::new(BTreeMap::new())),
+            entry_count: 0,
+            size: 0,
+            capacity,
         }
     }
 
-    /// Get the number of key-value pairs in the MemTable
-    pub(crate) fn len(&self) -> usize {
-        // Get the number of elements in the skip map
-        self.entries.len()
+    /// Inserts a key-value pair into the MemTable.
+    pub(crate) fn set(&mut self, key: Vec<u8>, value: Vec<u8>) -> io::Result<()> {
+        let mut entries = self.entries.lock().map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to acquire lock on MemTable entries.",
+            )
+        })?;
+
+        let size_change = key.len() + value.len();
+        entries.insert(key, value);
+
+        self.entry_count += 1;
+        self.size += size_change;
+
+        Ok(())
+    }
+
+    /// Get the value of the given key
+    pub(crate) fn get(&self, key: Vec<u8>) -> io::Result<Option<Vec<u8>>> {
+        let entries = self.entries.lock().map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to acquire lock on MemTable entries.",
+            )
+        })?;
+        Ok(entries.get(&key).cloned())
+    }
+
+    /// Remove the key-value entry of the given key.
+    pub(crate) fn remove(&mut self, key: Vec<u8>) -> io::Result<Option<Vec<u8>>> {
+        let mut entries = self.entries.lock().map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to acquire lock on MemTable entries.",
+            )
+        })?;
+
+        let value = entries.remove(&key);
+
+        if let Some(v) = &value {
+            self.entry_count -= 1;
+            self.size -= key.len() + v.len();
+        }
+
+        Ok(value)
+    }
+
+    /// Clears all key-value entries in the MemTable.
+    pub(crate) fn clear(&mut self) -> io::Result<()> {
+        let mut entries = self.entries.lock().map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to acquire lock on MemTable entries.",
+            )
+        })?;
+
+        entries.clear();
+        self.entry_count = 0;
+        self.size = 0;
+        Ok(())
+    }
+    pub(crate) fn get_capacity(&self) -> usize {
+        self.capacity
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tempfile::{tempdir, TempDir};
 
-    fn setup_test_env() -> (MemTable, TempDir) {
-        let temp_dir = tempdir().expect("Failed to create temporary directory");
-        let log_file_path = temp_dir.path().join("test_memtable.log");
-
-        let memtable_result = MemTable::new(&log_file_path);
-
-        assert!(memtable_result.is_ok());
-
-        let memtable = memtable_result.unwrap();
-
-        (memtable, temp_dir)
+    macro_rules! kv {
+        ($k:expr, $v:expr) => {
+            ($k.as_bytes().to_vec(), $v.as_bytes().to_vec())
+        };
     }
 
-    fn teardown_test_env(temp_dir: TempDir) {
-        temp_dir
-            .close()
-            .expect("Failed to delete temporary directory");
+    fn generate_dummy_kv_pairs() -> Vec<(Vec<u8>, Vec<u8>)> {
+        let mut pairs = Vec::new();
+
+        pairs.push(kv!("key1", "value1"));
+        pairs.push(kv!("key2", "value2"));
+        pairs.push(kv!("key3", "value3"));
+
+        pairs
     }
 
     #[test]
     fn create_empty_memtable() {
-        let (memtable, temp_dir) = setup_test_env();
-
-        assert_eq!(memtable.entries.len(), 0);
-
-        teardown_test_env(temp_dir);
+        let m = MemTable::new();
+        assert_eq!(m.size, 0);
+        assert_eq!(m.entry_count, 0);
     }
 
     #[test]
-    fn set_single_entry() {
-        let (mut memtable, temp_dir) = setup_test_env();
+    fn create_single_entry() {
+        let mut m = MemTable::new();
 
-        let key = b"key1";
-        let value = b"value1";
+        let (key, value) = generate_dummy_kv_pairs()
+            .pop()
+            .expect("Failed to get first key-value pair");
 
-        memtable.set(key, value);
+        m.set(key.clone(), value.clone());
 
-        assert_eq!(memtable.len(), 1);
-        assert_eq!(memtable.get(key), Some(Bytes::copy_from_slice(value)));
+        assert_eq!(m.entry_count, 1);
+        assert_eq!(m.size, key.len() + value.len());
 
-        teardown_test_env(temp_dir);
+        assert_eq!(m.get(key).unwrap().unwrap(), value);
     }
 
     #[test]
-    fn memtable_set_and_get() {
-        let (mut memtable, temp_dir) = setup_test_env();
+    fn set_and_get() {
+        let mut m = MemTable::new();
 
-        memtable.set(b"a", b"apple");
-        memtable.set(b"b", b"banana");
-        memtable.set(b"c", b"cheese");
-        memtable.set(b"d", b"dragon fruit");
-        memtable.set(b"e", b"elderberry fruit");
+        // setup dummy key-value pairs
+        let kv = generate_dummy_kv_pairs();
 
-        assert_eq!(memtable.get(b"a"), Some(Bytes::copy_from_slice(b"apple")));
-        assert_eq!(memtable.get(b"b"), Some(Bytes::copy_from_slice(b"banana")));
-        assert_eq!(memtable.get(b"c"), Some(Bytes::copy_from_slice(b"cheese")));
-        assert_eq!(
-            memtable.get(b"d"),
-            Some(Bytes::copy_from_slice(b"dragon fruit"))
-        );
-        assert_eq!(
-            memtable.get(b"e"),
-            Some(Bytes::copy_from_slice(b"elderberry fruit"))
-        );
+        // set all dummy key-value pairs to mem-table
+        let mut size = 0;
+        kv.iter().for_each(|(key, value)| {
+            m.set(key.clone(), value.clone());
 
-        assert_eq!(memtable.len(), 5);
+            size += key.len() + value.len();
 
-        teardown_test_env(temp_dir);
+            assert_eq!(m.get(key.clone()).unwrap().unwrap(), value.clone());
+        });
+
+        assert_eq!(m.entry_count, kv.len());
+        assert_eq!(m.size, size);
     }
 
     #[test]
-    fn remove_key_success() {
-        let (mut memtable, temp_dir) = setup_test_env();
+    fn set_and_remove() {
+        let mut m = MemTable::new();
 
-        memtable.set(b"a", b"apple");
-        let res = memtable.remove(b"a");
+        let kv = generate_dummy_kv_pairs();
 
-        assert!(res.is_ok());
-        assert_eq!(memtable.get(b"a"), None);
-        assert_eq!(memtable.len(), 0);
+        let mut size = 0;
 
-        teardown_test_env(temp_dir);
-    }
+        // set key-value
+        kv.iter().for_each(|(key, value)| {
+            m.set(key.clone(), value.clone());
+            size += key.len() + value.len();
+            assert_eq!(m.size, size);
+        });
 
-    #[test]
-    fn remove_non_exist_key_should_give_error() {
-        let (mut memtable, temp_dir) = setup_test_env();
+        // remove key-value
+        kv.iter().for_each(|(key, value)| {
+            m.remove(key.clone());
+            size -= key.len() + value.len();
+            assert_eq!(m.size, size);
+        });
 
-        memtable.set(b"key1", b"apple");
-        // key "a" doesn't exist, therefore should fail.
-        let res = memtable.remove(b"a");
-        assert!(res.is_err());
-
-        teardown_test_env(temp_dir);
+        assert_eq!(m.size, 0);
     }
 }
